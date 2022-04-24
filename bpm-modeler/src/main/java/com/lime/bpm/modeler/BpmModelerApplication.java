28440f03d97b33a3d2bb78e0731378dda51da8dd
lichaog <lichaog@yonyou.com>
2022-04-24 12:56:36 +0800

    init

---
+++ bpm-modeler/src/main/java/com/lime/bpm/modeler/BpmModelerApplication.java
@@ -0,0 +1,13 @@
+package com.lime.bpm.modeler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmModelerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpmModelerApplication.class, args);
+    }
+
+}
---
+++ bpm-modeler/src/test/java/com/lime/bpm/modeler/BpmModelerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lime.bpm.modeler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpmModelerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
